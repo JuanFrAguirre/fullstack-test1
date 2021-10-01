8da2a47ec0fa1f073baf154c1ed14e1a58838f3a
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2021-10-01 07:42:31 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'test1';
+export class AppComponent implements OnInit {
+  data: any
+  loading
+
+  constructor(public _http: HttpClient) {
+    this.loading = true
+  }
+  ngOnInit(): void {
+    this.getData()
+  }
+
+  getData() {
+    setTimeout(() => {
+      this._http.get('http://localhost:3001/api').subscribe((x) => {
+        this.data = x
+        this.loading = false
+      })
+    }, 700)
+  }
 }
